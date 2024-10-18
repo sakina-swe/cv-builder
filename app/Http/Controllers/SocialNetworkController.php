fdf159797ca206ef702cf799da797cb50bc54201
sakinamt <sakinamt24@gmail.com>
2024-10-18 21:27:43 +0500

    MAJOR: Install laravel, setup docker environment, create models, migrations, factories, seeders

---
+++ app/Http/Controllers/SocialNetworkController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSocialNetworkRequest;
+use App\Http\Requests\UpdateSocialNetworkRequest;
+use App\Models\SocialNetwork;
+
+class SocialNetworkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSocialNetworkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SocialNetwork $socialNetwork)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SocialNetwork $socialNetwork)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSocialNetworkRequest $request, SocialNetwork $socialNetwork)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SocialNetwork $socialNetwork)
+    {
+        //
+    }
+}
